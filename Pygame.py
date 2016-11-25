392df5e5fe83994e36f5a1a38dfe096964c3cdfe
Jessica Mathis <jbmathis@umich.edu>
2016-11-24 23:25:58 -0500

    commit 4

--- Pygame.py
+++ Pygame.py
@@ -1,3 +1,18 @@
-#I am starting my project
-#I am working on my project
-#This is for commit 3 
+import os
+import pygame
+pygame.init();
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Flappy Bird Adaptation')
+pygame.display.update()		#only updates portion specified
+
+gameExit = False
+while not gameExit:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+
+
+#required
+pygame.quit()
+quit()	
