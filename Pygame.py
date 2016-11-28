0da13cb1613ace61ad1c333c202e722f682b8cc1
Jessica Mathis <jbmathis@umich.edu>
2016-11-28 00:52:40 -0500

    commit 11

--- Pygame.py
+++ Pygame.py
@@ -7,9 +7,8 @@ white = (255,255,255)
 #position vars
 background_x = 0
 background_y = 0
-mich_x = 10
-mich_y = 10
-
+mich_x = 75
+mich_y = 225
 
 class Michigan(pygame.sprite.Sprite):
         WIDTH = 25
@@ -21,7 +20,6 @@ class Michigan(pygame.sprite.Sprite):
         def __init__(self, x, y, msec_to_climb, image):
                 super(Michigan, self).__init__()
                 self.image = image
-                self.image = pygame.transform.scale(self.image, (10,10))
                 self.rect = self.image.get_rect()
                 self.rect.x = x
                 self.rect.y = y
@@ -49,23 +47,33 @@ def main():
         #create the dictionary of photos
         images = load_images()
         background = images['Michigan_Wolverines_Field']
-        mich_y = 10
         player = Michigan(mich_x, mich_y, 2, images['block_m'])
 
+        clock = pygame.time.Clock()
+        time_elapsed = 0
+        
         gameExit = False
         while not gameExit:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 gameExit = True
-                        if event.type == pygame.KEYDOWN:
-                                if event.key == pygame.K_UP:
-                                        player.rect.y -= 10
+                if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                                player.rect.y -= 10
+                                
                 gameDisplay.blit(images['Michigan_Wolverines_Field'], (background_x, background_y))
                 gameDisplay.blit(images['block_m'], player.rect)
                 pygame.display.update()
-
                 player.update()
-                #print(player.rect)
+
+                time = clock.tick()
+                time_elapsed += time
+                if time_elapsed >= 250:
+                        player.rect.y += 20
+                        time_elapsed = 0
+
+                if player.rect.y == 475:
+                        gameExit == True
 	
 main()
 
